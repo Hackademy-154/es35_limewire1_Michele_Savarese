29d5b2aa2a2fa997842bd6c093f6e1bb730db55f
DragonautMk <michelesavarese1997@gmail.com>
2024-11-12 16:12:35 +0100

    finita la creazione del post con controlli live di livewire

--- resources/views/livewire/post-create-form.blade.php
+++ resources/views/livewire/post-create-form.blade.php
@@ -4,18 +4,34 @@
                 {{ session('success') }}
             </div>
         @endif
+        @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
         <div class="mb-3">
             <label for="title" class="form-label">
                 Titolo
             </label>
-            <input type="text" class="form-control" wire:model="title" id="title">
+            <input type="text" class="form-control @error('title') is-invalid
+
+            @enderror"  wire:model.blur="title" id="title">
+            {{-- {{$title}} --}}
         </div>
 
         <div class="mb-3">
             <label for="content" class="form-label">
                 Contenuto
             </label>
-            <textarea class="form-control" id="content" cols="30" rows="10" wire:model="content"></textarea>
+            <textarea class="form-control @error('content') is-invalid
+
+            @enderror" id="content" cols="30" rows="10" wire:model.blur="content"></textarea>
+            {{-- {{$content}} --}}
+
         </div>
         <div class="mt-3 text-center">
             <button type="submit" class="btn btn-info">
